2a350b824a5934324b6e7857f1ce49a827164b71
mohamedfasadkafafy-jpg <mohamedfasadkafafy@gmail.com>
2025-11-17 21:01:17 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,80 +1,35 @@
 document.getElementById('loginForm').addEventListener('submit', function(e){
     e.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
     const errorMsg = document.getElementById('errorMsg');
 
-    // Multiple users
+    // ALL USERS IN ONE ARRAY
     const users = [
-        {username: "mohamed farouk", password: "2910mf"},
-        {username: "aya hatem", password: "ay5410"},
-        {username: "aya mahmoud", password: "ma643"},
-        {username: "esraa anwar", password: "ea84521"},
-        {username: "amira elsherbini", password: "ae311154"},
-        {username: "aya medhat", password: "am84333328"},
-        {username: "jana yosri", password: "jy75432198765674"},
-        {username: "mahmoud wael", password: "mw6548876665444435"},
-        {username: "rafiq mahmoud", password: "rm7777654897654"},
-        {username: "mena elsayed", password: "me90856667438977"},
-        {username: "rahma ibrahim", password: "ri65487900008765444432"},
-        {username: "amr elshorbagy", password: "ae8888888800098965784"},
-        {username: "abrar hassouna", password: "ah7666890654478"},
-        {username: "zeina hani", password: "zh7654444367595"},
-        {username: "fatima walid", password: "fw7531008765"},
-        {username: "fatima ezzat", password: "fe80977754321165"},
-        {username: "maryam alamir", password: "ma543332786554"},
-        {username: "menna allah", password: "mal54322287699421"},
-        {username: "haneen wael", password: "hw555438709844"},
-        {username: "shahd yasser", password: "sy54447689043"},
+        {username: "mohamed farouk", password: "2910mf", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+        {username: "aya hatem", password: "ay5410", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+        {username: "aya mahmoud", password: "ma643", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+        {username: "esraa anwar", password: "ea84521", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+        {username: "amira elsherbini", password: "ae311154", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+        {username: "aya medhat", password: "am84333328", link: "https://mohamedfasadkafafy-jpg.github.io/site/"},
+
+        // Dana
+        {username: "Dana Amr", password: "Da536744", link: "https://mohamedfasadkafafy-jpg.github.io/OnlinePlatformda/"},
+
+        // Rh
+        {username: "Rh", password: "Rh1", link: "https://mohamedfasadkafafy-jpg.github.io/OnlinePlatformra/"}
     ];
 
-    const validUser = users.find(user => user.username === username && user.password === password);
+    // FIND USER
+    const validUser = users.find(user =>
+        user.username.toLowerCase() === username.toLowerCase() &&
+        user.password === password
+    );
 
     if(validUser){
-        window.location.href = "https://mohamedfasadkafafy-jpg.github.io/site/"; // Change this to the target URL
+        window.location.href = validUser.link;
     } else {
         errorMsg.textContent = "Could not find your account";
     }
 });
-document.getElementById('loginForm').addEventListener('submit', function(e){
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const errorMsg = document.getElementById('errorMsg');
-
-    // Multiple users
-    const users = [
-        {username: "Dana Amr", password: "Da536744"},
-        ];
-
-    const validUser = users.find(user => user.username === username && user.password === password);
-
-    if(validUser){
-        window.location.href = "https://mohamedfasadkafafy-jpg.github.io/OnlinePlatformda/"; // Change this to the target URL
-    } else {
-        errorMsg.textContent = "Could not find your account";
-    }
-});
-document.getElementById('loginForm').addEventListener('submit', function(e){
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const errorMsg = document.getElementById('errorMsg');
-
-    // Multiple users
-    const users = [
-        {username: "Rh", password: "Rh1"},
-        ];
-
-    const validUser = users.find(user => user.username === username && user.password === password);
-
-    if(validUser){
-        window.location.href = "https://mohamedfasadkafafy-jpg.github.io/OnlinePlatformra/"; // Change this to the target URL
-    } else {
-        errorMsg.textContent = "Could not find your account";
-    }
-});
-
